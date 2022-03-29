01a3682866d6967d767d7b66f49aff470aa5eea2
Samnas <samucasnascimento@hotmail.com>
2022-03-29 17:31:03 -0300

    SERVICES: fix baseURL

--- src/pages/Food/index.jsx
+++ src/pages/Food/index.jsx
@@ -8,6 +8,7 @@ const Food = () => (
   <>
     <Header />
     <SearchFilters />
+
     <Footer />
   </>
 );
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const apiFood = axios.create({ baseURL: 'www.themealdb.com/api/json/v1/1/' });
+export const apiFood = axios.create({ baseURL: 'https://www.themealdb.com/api/json/v1/1/' });
 export const apiDrinks = axios.create(
   { baseURL: 'www.thecocktaildb.com/api/json/v1/1/' },
 );
