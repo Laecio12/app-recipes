01f20c847efa8415f9c99ab96d1b61a8157d04e2
Samnas <samucasnascimento@hotmail.com>
2022-03-29 17:05:06 -0300

    FEAT: Food - add SearchFilters

--- src/pages/Food/index.jsx
+++ src/pages/Food/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 // import { Container } from './styles';
 import Header from '../../components/Header/index';
 import Footer from '../../components/Footer/index';
+import SearchFilters from '../../components/SearchFilters/index';
 
 const Food = () => (
   <>
     <Header />
-    <h1> Food </h1>
+    <SearchFilters />
     <Footer />
   </>
 );
