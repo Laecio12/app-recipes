0b3a6aaa96986aaaab1fb00f48adec1e539a7ca2
Samnas <samucasnascimento@hotmail.com>
2022-03-29 16:54:25 -0300

    FEAT: Food - add Footer

--- src/pages/Food/index.jsx
+++ src/pages/Food/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Container } from './styles';
+// import { Container } from './styles';
 import Header from '../../components/Header/index';
+import Footer from '../../components/Footer/index';
 
 const Food = () => (
   <>
     <Header />
     <h1> Food </h1>
+    <Footer />
   </>
 );
 
