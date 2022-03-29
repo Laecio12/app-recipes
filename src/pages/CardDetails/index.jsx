45facc7371e519c60cc6a3184942dee1186c9530
Marcelle Monteiro <monteiro.bio@outlook.com>
2022-03-29 20:22:20 -0300

    feat details page

---
+++ src/pages/CardDetails/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const CardDetails = () => (
+  <Container>
+    <img src="" alt="food" data-testid="recipe-photo" />
+    <h1 data-testid="recipe-title">Title // Mudar para api result</h1>
+
+  </Container>
+);
+
+export default CardDetails;
