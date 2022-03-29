e25d5243eaec4c1eba3e82187b16ca7c672c6b3c
gabenerys <gabe.nery@live.com>
2022-03-28 21:12:47 -0300

    Modifying index

---
+++ src/components/DoneRecipesCard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const DoneRecipesCard = () => {
+  return (
+    <Container>
+      <h1> DoneRecipesCard </h1>
+    </Container>
+  );
+}
+
+export default DoneRecipesCard;
