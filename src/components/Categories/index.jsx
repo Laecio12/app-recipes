3d34065134892e9100b9b95326e37a1c2a19ec12
gabenerys <gabe.nery@live.com>
2022-03-28 21:09:04 -0300

    Setting Folders

---
+++ src/components/Categories/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Categories = () => {
+  return (
+    <Container>
+      <h1> Categories </h1>
+    </Container>
+  );
+}
+
+export default Categories;
+
