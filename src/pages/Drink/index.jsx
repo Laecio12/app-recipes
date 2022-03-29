dc13aa293db1fe82ca2cc824d8b09d0ef37bdd9e
Samnas <samucasnascimento@hotmail.com>
2022-03-29 16:51:43 -0300

    FEAT: Food - creating Component

---
+++ src/pages/Drink/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { submitBtn } from './styles';
+
+
+const Drink = () => {
+  return (
+    <Container>
+      <h1> Drink </h1>
+    </Container>
+  );
+}
+
+export default Drink;
---
+++ src/pages/Food/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from './styles';
+import Header from '../../components/Header/index';
+import SearchFilters from '../../components/SearchFilters/index';
+import 
+
+const Food = () => (
+  <>
+    <Header />
+    <SearchFilters />
+    <h1> Food </h1>
+    <Footer />
+  </>
+);
+
+export default Food;
