e5f3944c59550e1ac1df15a86d7246b31baf1bc3
Marcelle Monteiro <monteiro.bio@outlook.com>
2022-03-30 13:19:43 -0300

    feat create test for footer

---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../../utils/renderWithRouter';
+import Footer from '.';
+
