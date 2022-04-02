00c8c485364b8404aa50e9db84e611fd7c8cc56e
Samnas <samucasnascimento@hotmail.com>
2022-04-02 18:30:11 -0300

    STYLES: fix-lint

--- src/pages/Drink/styles.js
+++ src/pages/Drink/styles.js
@@ -1,13 +1,7 @@
 import styled from 'styled-components';
 
-<<<<<<< HEAD
 export const submitBtn = styled.div`
 
 `;
-=======
 export const Container = styled.div`
 `;
-
-export const Containerw = styled.div`
-`;
->>>>>>> c3ed1f79ec5a15ed191c0f262feff682a9e2f784
