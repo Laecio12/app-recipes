de5afb8ff6a35fac2b96b7857497fb0f79609f32
Samnas <samucasnascimento@hotmail.com>
2022-04-05 18:49:06 -0300

    STYLE: FoodPage, styling button

--- src/pages/Food/styles.js
+++ src/pages/Food/styles.js
@@ -4,4 +4,23 @@ export const Container = styled.div`
 `;
 
 export const FoodPageContainer = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    section:first-of-type {
+    display: flex;
+    width: 90vw;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    flex-direction: row;
+    margin-top: 10px;
+    }
+
+    button {
+    padding: 2px;
+    width: 70px;
+    margin: 3px 0;
+    }
+
 `;
