d19beb78e41d6c3e0e71897807e19820add3d212
Samnas <samucasnascimento@hotmail.com>
2022-04-07 00:29:34 -0300

    Excluding theme importer from APP

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { FoodsProvider } from './hooks/useFoods';
